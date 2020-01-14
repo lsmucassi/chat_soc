9da2b7046c2d01c9070acd4bd1e3eeda55f5fb5b
lsmucassi <lindasmucassi@gmail.com>
2020-01-14 15:16:49 +0200

    client: main

--- client.py
+++ client.py
@@ -13,3 +13,16 @@ server.connect((IP_address, Port))
 while True:
 
     socket_list - [sys.stdin, server]
+
+    read_sockets, write_sockets, err_sockets = select.select(sockets_list, [], [])
+
+    for socks in read_socket:
+        if sockks == server:
+            message = socks.recv(2048)
+            print message
+        else:
+            message = sys.stdin.readline()
+            sys.stdout.write("<You>")
+            sys.stdout.write(message)
+            sysy.stdout.flush()
+server.close()
